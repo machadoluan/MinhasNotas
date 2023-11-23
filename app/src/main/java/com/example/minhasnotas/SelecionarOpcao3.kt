7a24fe1fa575ec1359281d617e7917d1bcf89223
Luan Machado <machado.luandealmeida@gmail.com>
2023-11-23 17:31:54 -0300

    initial commit

---
+++ app/src/main/java/com/example/minhasnotas/SelecionarOpcao3.kt
@@ -0,0 +1,11 @@
+package com.example.minhasnotas
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SelecionarOpcao3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selecionar_opcao3)
+    }
+}
---
+++ app/src/main/java/com/example/minhasnotas/TrimestreOp1.kt
@@ -0,0 +1,11 @@
+package com.example.minhasnotas
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TrimestreOp1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trimestre_op1)
+    }
+}
